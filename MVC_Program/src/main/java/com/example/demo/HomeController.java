ecc17f255a01e6b7a3b4cdfc86932883276348c0
Ganesh <ganeshbaringe99@gmail.com>
2025-01-10 11:40:54 +0530

    Task 1 Completed

--- MVC_Program/src/main/java/com/example/demo/HomeController.java
+++ MVC_Program/src/main/java/com/example/demo/HomeController.java
@@ -5,22 +5,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-	@Controller
-	public class HomeController {
+@Controller
+public class HomeController {
 
-	   @GetMapping("/test") 
-	    public String home(Model model) {
-	        model.addAttribute("title", "Home Page");
-	        model.addAttribute("welcomeMessage", "Welcome to Thymeleaf MVC!");
-	        return "home";
-	    }
-	    @GetMapping("/wel")
-	    public String welcome(Model model) {
-	        model.addAttribute("title", "welcome Page");
-	        model.addAttribute("welcomeMessage", "Welcome to Thymeleaf MVC!");
-	        return "welcome";
-	    }
+	@GetMapping("/test")
+	public String home(Model model) {
+		model.addAttribute("title", "Home Page");
+		model.addAttribute("welcomeMessage", "Welcome to Thymeleaf MVC!");
+		System.out.println("Task1");
+		return "home";
 	}
 
-
-
+	@GetMapping("/wel")
+	public String welcome(Model model) {
+		model.addAttribute("title", "welcome Page");
+		model.addAttribute("welcomeMessage", "Welcome to Thymeleaf MVC!");
+		return "welcome";
+	}
+}
